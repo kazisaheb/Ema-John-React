4f2c091dabe028c9daa627a138aba0ac9b179899
Kazi Saheb <kzsaheb@gmail.com>
2022-03-22 15:36:43 +0600

    Cart button CSS

--- src/Components/Orders/Orders.js
+++ src/Components/Orders/Orders.js
@@ -5,6 +5,14 @@ export default function Orders() {
   return (
     <div className='Orders'>
       <h3>Order Summery</h3>
+      <p>Selected Items: </p>
+      <p>Total Price: $</p>
+      <p>Shipping Charge: </p>
+      <p>Tax: $</p>
+      <h4>Grand Total: $</h4>
+      <button className='clearCart'>Clear Cart</button>
+      <br />
+      <button className='reviewOrder'>Review Order</button>
     </div>
   )
 }
